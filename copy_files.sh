49c5e30613b8c4daaa7268dde77a157524ca0605
Maxim Romanov <romanov.maxim@gmail.com>
2022-09-02 13:17:53 +0200

    working version of the highlighting scheme for gedit

---
+++ copy_files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp EIS1600.lang /opt/homebrew/Cellar/gtksourceview4/4.8.3/share/gtksourceview-4/language-specs/EIS1600.lang
+cp EIS1600.xml /opt/homebrew/Cellar/gtksourceview4/4.8.3/share/gtksourceview-4/styles/EIS1600.xml
+
+ECHO "Die Dateien wurden kopiert!"
